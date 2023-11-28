c9b8a8413c736ab28ac1e0cf69867a0044235ec9
Abdulrahman Ahmed <89032128+Abdulrahmanaw7@users.noreply.github.com>
2023-11-28 13:18:32 +0200

    Add project files.

---
+++ TwilioSMS/Controllers/SMSController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TwilioSMS.Services;
+using TwilioSMS.ViewModels.Dtos;
+
+namespace TwilioSMS.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SMSController(ISMSService smsService) : ControllerBase
+    {
+        private readonly ISMSService _smsService= smsService;
+
+
+        [HttpPost("send")]
+        public IActionResult Send(SendSMSDto dto)
+        {
+            var result = _smsService.Send(dto.MobileNumber, dto.Body);
+
+            if (!string.IsNullOrEmpty(result.ErrorMessage))
+                return BadRequest(result.ErrorMessage);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ TwilioSMS/Services/SMSService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using TwilioSMS.Helpers;
+
+namespace TwilioSMS.Services
+{
+    public class SMSService(IOptions<TwilioSettings> twilio) : ISMSService
+    {
+        private readonly TwilioSettings _twilio = twilio.Value;
+
+
+        public MessageResource Send(string mobileNumber, string body)
+        {
+            TwilioClient.Init(_twilio.AccountSID, _twilio.AuthToken);
+
+            var result = MessageResource.Create(
+                    body: body,
+                    from: new Twilio.Types.PhoneNumber(_twilio.TwilioPhoneNumber),
+                    to: mobileNumber
+                );
+
+            return result;
+        }
+    }
+}
